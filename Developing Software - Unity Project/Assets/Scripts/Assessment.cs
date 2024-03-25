f62fde8f15045f6c6371cb284be8b0f510345275
2144619 <2144619@fife.ac.uk>
2024-03-25 19:44:48 +0000

    I think I've cracked it

--- Developing Software - Unity Project/Assets/Scripts/Assessment.cs
+++ Developing Software - Unity Project/Assets/Scripts/Assessment.cs
@@ -32,8 +32,8 @@ public class Assessment : MonoBehaviour
     public void PrepareData()
     {
         //Get playerScore & playerName from PlayerPrefs
-        playerScore = PlayerPrefs.GetInt("playerScore", 0);
-        playerName = PlayerPrefs.GetString("playerName", "");
+        playerScore = PlayerPrefs.GetInt("playerScore");
+        playerName = PlayerPrefs.GetString("playerName");
 
         //INITIALISE newScore and newName variables to be equal to playerScore and playerName
         int newScore = playerScore;
@@ -46,24 +46,30 @@ public class Assessment : MonoBehaviour
             int highScore = PlayerPrefs.GetInt("highScore" + scoreIndex.ToString());
             string highScoreName = PlayerPrefs.GetString("highScoreName" + scoreIndex.ToString());
 
+
+
             //IF newScore is greater than highScore THEN
             if (newScore > highScore) 
             {
                 //SET highScoreAchieved to true
                 highScoreAchieved = true;
 
-                string newScoreName = newName;
+                //string newScoreName = newName;
 
                 //WRITE newScore and newScoreName variables into Unity PlayerPrefs forthe high score and high score name at this scoreIndex.
 
                 PlayerPrefs.SetInt("highScore" + scoreIndex.ToString(), newScore);
                 PlayerPrefs.SetString("highScoreName" + scoreIndex.ToString(), newName);
 
-                PlayerPrefs.Save();
+
+                
                 //SET newScore equal to highScore
                 newScore = highScore;
                 //SET newName equal to highScoreName
-                newName = newScoreName;
+                newName = highScoreName;
+                
+                //SAVE PlayerPrefs
+                //PlayerPrefs.Save();
             }
         }
     }
@@ -83,15 +89,18 @@ public class Assessment : MonoBehaviour
             messageDisplay.text = "Better luck next time!";
         }
         //DISPLAY playerName and playerScore to the playerNameDisplay and playerScoreDisplay text objects 
-        playerName = playerNameDisplay.text;
+        playerNameDisplay.text = playerName;
+
         playerScoreDisplay.text = playerScore.ToString();
 
         //FOR scoreIndex from 0 to 3
-        for (int scoreIndex = 0; scoreIndex < 3; scoreIndex++)
+        for (int scoreIndex = 0; scoreIndex < 3; ++scoreIndex)
         {
             //READ highScore and highScoreName variables from Unity PlayerPrefs using the scoreIndex
-            int highScore = PlayerPrefs.GetInt("highScore", 0);
-            string highScoreName = PlayerPrefs.GetString("highScoreName", "");
+
+            int highScore = PlayerPrefs.GetInt("highScore" + scoreIndex.ToString());
+
+            string highScoreName = PlayerPrefs.GetString("highScoreName" + scoreIndex.ToString());
 
             //CALL DisplayScore() with scoreIndex, highScore, and highScoreName
             DisplayScore(scoreIndex, highScore, highScoreName);
@@ -107,23 +116,24 @@ public class Assessment : MonoBehaviour
         if (score > 0)
         {
             //2.9.2. DISPLAY score to the highScoreDisplays at index
-            //highScoreDisplay[index].text = score.ToString();
-            highScoreNameDisplays[index].text = score.ToString();
+            highScoreDisplays[index].text = score.ToString();
 
 
         }
         //2.9.3. ELSE
         else
-        {
+        {            
             //2.9.4. DISPLAY the empty string to the highScoreDisplays at index.
-            highScoreDisplays[index].text = name;
+
+            highScoreDisplays[index].text = "";
 
         }
         //2.9.5. ENDIF
 
 
         //2.9.6. DISPLAY name to the highScoreNameDisplays at index
-        name = highScoreNameDisplays[index].ToString();
+        highScoreNameDisplays[index].text = name;
+
     }
 
     // Start is called before the first frame update
